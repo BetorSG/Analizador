50edc1534c0195b9fbaf85fb3e3603ea10691429
Roberto Rosas <betorsglp@gmail.com>
2019-08-05 13:20:46 -0500

    Se agrego el código de Sensor

--- Borrador_analizador.ino
+++ Borrador_analizador.ino
@@ -1,6 +1,18 @@
 #include <LiquidCrystal.h> // Incluye la Libreria LiquidCrystal
 LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Crea un Objeto LC. Parametros: (rs, enable, d4, d5, d6, d7)
-void setup() {   
+const int EchoPin = 11;
+const int TriggerPin = 12;
+const int LedPinVerde = 8;
+const int LedPinBlanco = 9;
+float distancia;
+long tiempo;
+
+void setup() {  
+  pinMode(LedPinVerde, OUTPUT);
+  pinMode(LedPinBlanco, OUTPUT);
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+    
   lcd.begin(16,2); // Inicializa la interface para el LCD screen, and determina sus dimensiones (ancho y alto) del display
   lcd.print("   Bienvenido");
   delay(3000);
@@ -33,6 +45,38 @@ void loop() {
   lcd.print("Analizando..."); 
   delay(1000);   
   lcd.clear();
-  delay(1000);
-    
+  delay(1000);  
+
+   
+  digitalWrite(TriggerPin, HIGH);  //se envía un pulso para activar el sensor
+  delayMicroseconds(10);
+  digitalWrite(TriggerPin, LOW);
+
+  // medimos el pulso de respuesta
+  tiempo = (pulseIn(EchoPin, HIGH)/2); 
+  
+  // dividido por 2 por que es el 
+  // tiempo que el sonido tarda
+  // en ir y en volver
+  // ahora calcularemos la distancia en cm
+  // sabiendo que el espacio es igual a la velocidad por el tiempo
+  // y que la velocidad del sonido es de 343m/s y que el tiempo lo 
+  // tenemos en millonesimas de segundo
+  
+  distancia = float(tiempo * 0.0343);
+  Serial.print("Distancia: "); // imprime la distancia en el Monitor Serie
+  Serial.println(distancia);
+  
+  if (distancia >= 50) {
+    digitalWrite(LedPinVerde , HIGH);
+    digitalWrite(LedPinBlanco , LOW);
+  }
+  else if(distancia <= 49){
+    digitalWrite(LedPinVerde , LOW);
+    digitalWrite(LedPinBlanco , HIGH);
+        
+  }  
+  
+  delay(500);
+
  }
