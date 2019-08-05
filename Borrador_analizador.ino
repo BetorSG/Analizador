54dd1a137a5fb52a4124b64fc57230a20546d18d
Roberto Rosas <betorsglp@gmail.com>
2019-08-05 12:13:39 -0500

    Despliegue de mensaje en el LCD

--- Borrador_analizador.ino
+++ Borrador_analizador.ino
@@ -1,11 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h> // Incluye la Libreria LiquidCrystal
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Crea un Objeto LC. Parametros: (rs, enable, d4, d5, d6, d7)
+void setup() {   
+  lcd.begin(16,2); // Inicializa la interface para el LCD screen, and determina sus dimensiones (ancho y alto) del display
+  lcd.print("   Bienvenido");
+  delay(3000);
+  lcd.clear();
+  delay(3000);
 
+  lcd.print(" Roberto Rosas"); // Imprime "LCD Tutorial" sobre el LCD
+  delay(3000);   // 3 segundos de espera
+  lcd.setCursor(0,1); // Seteamos la ubicacion texto 0 linea 1 que sera escrita sobre el LCD
+  
+  lcd.print("    FI UNAM");
+  delay(4000);
+  lcd.clear(); // Limpia la pantalla
+  delay(1500);
+
+  lcd.print("Quantum Analizer");
+   
+  lcd.setCursor(0,1); 
+  lcd.print("Bioresonance");
+  delay(5000);
+  lcd.clear(); // 
+  delay(2000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-//Hacer las pruebas del despliegue de mensaje en la pantalla lcd
-//Pantalla lcd 16x2
+void loop() {   
+  /*lcd.blink(); // Displayamos el Blinking del Cursor sobre el LCD
+  delay(3000);
+  lcd.noBlink();*/ // Apagamos el Blinking del Cursor sobre el LCD
+  
+  lcd.print("Analizando..."); 
+  delay(1000);   
+  lcd.clear();
+  delay(1000);
+    
+ }
